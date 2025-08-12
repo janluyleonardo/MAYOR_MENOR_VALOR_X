15174ef7fc8809a9a301b50cdaf3b39a6840e9b0
janluyleonardo <janluy.moreno@gmail.com>
2025-08-12 13:41:58 -0500

    carga de archivos adicionales

---
+++ src/mayoromenor/MayoroMenor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mayoromenor;
+
+/**
+ *
+ * @author 15_TECFON2
+ */
+public class MayoroMenor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz IN = new Interfaz();
+        IN.setVisible(true);
+    }
+    
+}
